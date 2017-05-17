3e80d82d599f9064fab1c582269c0dfcdd9b4cdd
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-05-17 13:36:56 -0700

    Pulling storeName from StorePicker input onSubmit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault();
+    // console.log("Went to the store!");
+
+    console.log(this.storeInput.value);
+
+    // grab text from input
+    // Transition from / to /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text"
+               required placeholder="Store Name"
+               defaultValue={getFunName()}
+               ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store</button>
       </form>
     )
