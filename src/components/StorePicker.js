11e7ded13a14e356a38ead4d857fec7714e6c037
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-05-16 21:57:42 -0700

    Imported css file in index.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <h1>Hello</h1>
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
