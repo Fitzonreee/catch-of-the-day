bb480cad203379a5fc3c6cfc7df8810cd40c76f2
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-05-20 16:56:22 -0700

    Adding Fish to Order

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,8 @@ class App extends React.Component {
     // bind the addFish method to the app itself
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+
     // Get Initial State
     this.state = {
       fishes: {},
@@ -40,6 +42,17 @@ class App extends React.Component {
     })
   }
 
+  addToOrder(key) {
+    console.log(key);
+
+    // take a copy of state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -47,7 +60,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
             {
-              Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              Object.keys(this.state.fishes)
+                    .map(key => <Fish key={key}
+                                      index={key}
+                                      details={this.state.fishes[key]}
+                                      addToOrder={this.addToOrder}
+                                />)
             }
           </ul>
         </div>
